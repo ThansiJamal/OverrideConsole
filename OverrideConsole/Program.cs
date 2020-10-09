a18235fc319593f0c7dd0cb73f15afaa0ba26752
ThansiJamal <thansijamal18@gmail.com>
2020-10-09 08:59:36 -0700

    example of override

---
+++ OverrideConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideConsole
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+        class Dog : Animal
+        {
+            public override void eat()
+            {
+                Console.WriteLine("Eating bread...");
+            }
+
+        }
+        class TestOverriding
+        {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.eat();
+            Console.ReadKey();
+        }
+
+    }
+}
+   
+
